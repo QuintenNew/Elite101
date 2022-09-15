99d2908e90b16e89b4f5fad8b53279ed6677fdce
Quinten Newbern <>
2022-09-15 03:12:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+user_input = input('What would you like to talk about? Please enter the corrosponding number with your desired talking topic. Cars: 1 Food: 2 Dreams: 3')
+
+topics =['Cars', 'Food', 'Dreams']
+
+if user_input == '1':
+  dream_car = input('What is your dream car?')
+  print('WOW thats a cool car hope you can get it one day!')
+
+if user_input == '2':
+  fav_food = input('What is your favorite food?')
+  print('WOW that sounds yummy I want to try some one day!')
+
+if user_input == '3':
+  dreams = input('What are your dreams for the future?')
+  print("WOW you're so ambitious I hope your dreams come ture!")
